d13489fddd939616aaa8c1b0a4e35b54d3d03930
pbies <39916932+pbies@users.noreply.github.com>
2018-06-04 18:16:41 +0200

    rar added, fixes
    no empty passwords
    no filename encryption
    zip stop stopwatch

--- rars_progress_time.ps1
+++ rars_progress_time.ps1
@@ -3,19 +3,21 @@ Write-Host `n`n`n`n`n`n`n
 
 $passwords = Get-Content "passwords.txt"
 $count = $passwords.Length
-$7ZipPath = "C:\Program Files\7-Zip\7z.exe"
+# $rar = "c:\Program Files\WinRAR\WinRAR.exe"
+# $rar = "c:\Program Files\WinRAR\Rar.exe"
+$rar = "c:\Program Files\WinRAR\UnRAR.exe"
 
-Get-ChildItem "." -Filter *.zip |
+Get-ChildItem "." -Filter *.rar |
 Foreach-Object {
 	$i = 0
 	$sw = [Diagnostics.Stopwatch]::StartNew()
 	Write-Host Processing file: $_
 	foreach ($password in $passwords)
 	{
-		& $7ZipPath "t" $_ "-p$password" > $null 2> $null
+		& "$rar" 't' '/inul' "$_" "/p$password"
 		if ($?)
 		{
-			Write-Host "Valid password: "$password
+			Write-Host "Valid password:"$password
 			[console]::beep(500,300)
 			$sw.Stop()
 			Write-Host time = $sw.Elapsed.ToString()
