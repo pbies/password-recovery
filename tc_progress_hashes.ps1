abca4a05b1496b9ed5988a6c41d97719997e7466
pbies <39916932+pbies@users.noreply.github.com>
2018-06-06 13:00:29 +0200

    added TC container, many fixes for TC script
    TC: dismount first, dismount at last, change password variable name,

--- tc_progress_hashes.ps1
+++ tc_progress_hashes.ps1
@@ -93,41 +93,55 @@ $file = "container.tc"
 
 function suc($p) {
 	Write-Host "Valid password: $p"
-	Write-Output "$_`:$password" >> potfile.txt
+	Write-Output "$file`:$p" >> potfile.txt
+	Write-Host -nonewline "Dismounting..."
+	$process = (Start-Process -FilePath "$tc" -ArgumentList "/s /q /dz /f" -PassThru -Wait)
+	Write-Host "done"
 	[console]::beep(500,300)
 	pause
 	exit
 }
 
 function test($p) {
-	$process = (Start-Process -FilePath "$tc" -ArgumentList "/a /s /q /l z /v $file /p $p" -PassThru -Wait)
+	$process = (Start-Process -FilePath "$tc" -ArgumentList "/a /s /q /l z /m ro /v $file /p $p" -PassThru -Wait)
 	if ($process.ExitCode -eq 0)
 	{
-		suc($password)
+		suc($p)
 	}
 }
 
 $i = 0
 Write-Host "Processing file: $file"
+If (Test-Path z:\)
+{
+	Write-Host -nonewline "Dismounting..."
+	$process = (Start-Process -FilePath "$tc" -ArgumentList "/s /q /dz /f" -PassThru -Wait)
+	Write-Host "done"
+}
+
 foreach ($password in $passwords)
 {
 	test($password)
+
 	$hash = [System.Text.Encoding]::ASCII.GetBytes($password) | Get-CRC32
 	$hash = [System.Convert]::ToString($hash,16)
 	while ($hash.length -lt 8) { $hash="0$hash" }
 	test($hash)
+
 	$md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
 	$ascii = [System.Text.Encoding]::ASCII.GetBytes($password)
 	$hash = [System.BitConverter]::ToString($md5.ComputeHash($ascii))
 	$hash = $hash -replace "-", ""
 	$hash = $hash.ToLower()
 	test($hash)
+
 	$sha256 = new-object -TypeName System.Security.Cryptography.SHA256CryptoServiceProvider
 	$ascii = [System.Text.Encoding]::ASCII.GetBytes($password)
 	$hash = [System.BitConverter]::ToString($sha256.ComputeHash($ascii))
 	$hash = $hash -replace "-", ""
 	$hash = $hash.ToLower()
 	test($hash)
+
 	$i++
 	Write-Progress -Activity "Testing passwords" -status "$i/$count done" -percentComplete ($i/$count*100)
 }
